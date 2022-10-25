e96e0e9273d9efdf22e6c8d3477e3453e8a830dd
Austin Walela <austinmasibo@gmail.com>
2022-10-25 12:34:45 +0300

    Create login page template

--- packages/client/src/pages/Login/Login.tsx
+++ packages/client/src/pages/Login/Login.tsx
@@ -1,5 +1,86 @@
+import * as React from 'react'
+import splash from '../../assets/splash.jpg'
+import logo from '../../assets/wallet.png'
+
 function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <React.Fragment>
+      <div className="min-h-full w-full h-screen flex">
+        <div className="hidden lg:block relative w-0 flex-1">
+          <img
+            className="absolute inset-0 h-full min-h-screen w-full object-cover"
+            src={splash}
+            alt="Abstract splash image"
+          />
+        </div>
+        <div className="flex-1 flex flex-col bg-gray-100 justify-center py-12 lg:flex-1">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+              <img className="h-10 w-auto" src={logo} alt="FinBank logo" />
+              <h2 className="mt-8 text-3xl font-bold text-gray-800">
+                Sign in to your account
+              </h2>
+            </div>
+            <div className="mt-8">
+              <div className="mt-6">
+                <form className="space-y-6">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700">
+                      Email
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="email"
+                        autoComplete="email"
+                        className="input-primary"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700">
+                      Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="password"
+                        autoComplete="current-password"
+                        className="input-primary"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <input
+                        id="remember-me"
+                        name="remember-me"
+                        type="checkbox"
+                        className="h-4 w-4 text-finmidpurple focus:ring-finmindpurple-light border-gray-300 rounded"
+                      />
+                      <label
+                        htmlFor="remember-me"
+                        className="ml-2 block text-sm text-gray-900">
+                        Stay signed in for a week
+                      </label>
+                    </div>
+                  </div>
+
+                  <div>
+                    <button type="submit" className="btn-primary">
+                      Sign in
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
-export default Login;
+export default Login
--- packages/client/tailwind.config.cjs
+++ packages/client/tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        finmidyellow: {
+          DEFAULT: '#ffde00',
+        },
+        finmidpurple: {
+          light: '#7b74ff',
+          DEFAULT: '#615aff',
+          dark: '#4841e6',
+        },
+      },
+    },
   },
-  plugins: [require("@tailwindcss/forms")],
-};
+  plugins: [require('@tailwindcss/forms')],
+}
