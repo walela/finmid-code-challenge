c8b83d6f342b82fcc5a2cc343bd439426fc2a4a9
Austin Walela <austinmasibo@gmail.com>
2022-10-25 13:43:13 +0300

    Add axios default and auth instances

--- packages/client/src/utils/axios.ts
+++ packages/client/src/utils/axios.ts
@@ -1,3 +1,34 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import { getUserInfo } from '../context/AuthContext'
 
-export { axios }
+let config: Record<string, string> = {
+  baseURL: 'http://localhost:3000/',
+}
+
+const axiosInstance = axios.create(config)
+const axiosWithAuth = axios.create(config)
+
+axiosWithAuth.interceptors.request.use(
+  function (config: AxiosRequestConfig) {
+    const token = getUserInfo().token
+    config.headers = config.headers ?? {}
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response.data
+  },
+  async function (error) {
+    return error
+  }
+)
+export default axiosInstance
+export { axiosWithAuth }
