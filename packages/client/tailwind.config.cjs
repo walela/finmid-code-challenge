943b014f62e87e8896a38a8e60411561ff6369ea
Austin Walela <austinmasibo@gmail.com>
2022-10-25 12:43:24 +0300

    Tweak default sans font

--- packages/client/tailwind.config.cjs
+++ packages/client/tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         finmidyellow: {
           DEFAULT: '#ffde00',
