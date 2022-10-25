36778d58175f22b74a652b58dda09e70df46f5b2
Austin Walela <austinmasibo@gmail.com>
2022-10-25 22:44:22 +0300

    Initial transactions data fetching

--- packages/client/src/pages/Dashboard/Dashboard.tsx
+++ packages/client/src/pages/Dashboard/Dashboard.tsx
@@ -177,7 +177,7 @@ export default function Dashboard() {
               <div className="flex-1 flex">
                 <form className="w-full flex md:ml-0" action="#" method="GET">
                   <label htmlFor="search-field" className="sr-only">
-                    Search
+                    Search 
                   </label>
                   <div className="relative w-full text-gray-400 focus-within:text-gray-600">
                     <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
@@ -186,7 +186,7 @@ export default function Dashboard() {
                     <input
                       id="search-field"
                       className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                      placeholder="Search"
+                      placeholder="Search by merchant name"
                       type="search"
                       name="search"
                     />
